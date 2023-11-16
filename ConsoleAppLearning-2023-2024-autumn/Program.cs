e406a7fcebe3b7b5da040c7d1f04f5b75adca7c6
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2023-11-16 19:18:08 +0200

    Add project files.

---
+++ ConsoleAppLearning-2023-2024-autumn/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+Console.WriteLine("I start learning functions and variables today.");
+
+int MultiplyTwoNumbers(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+Console.WriteLine(MultiplyTwoNumbers(5, 4));
+
+string SayHello()
+{
+    return "Hey hey!";
+}
+
+Console.WriteLine(SayHello());
+
+
+void SendEmail(string address)
+{
+    // Procedure for sending an email
+    Console.WriteLine($"E-mail to {address} is sent.");
+}
+
+SendEmail("customer@someemail.com");
---
+++ ConsoleAppLearning-2023-2024-autumn/Wizard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLearning_2023_2024_autumn
+{
+    internal class Wizard
+    {
+        string name;
+        int age;
+        int energy;
+        int health;
+
+        void Talk()
+        {
+            Console.WriteLine($"{name} is talking");
+        }
+
+        void Heal()
+        {
+            Console.WriteLine($"{name} is healing.");
+        }
+    }
+}
