c21e64e89ce93139b39c9ad3525814114cb52947
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2023-11-23 18:55:00 +0200

    Dragon class added

---
+++ ConsoleAppLearning-2023-2024-autumn/Dragon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLearning_2023_2024_autumn
+{
+    internal class Dragon
+    {
+        public string name;
+        public int age;
+        public int energy;
+        public int health;
+
+        public void Talk()
+        {
+            Console.WriteLine($"{name} is talking");
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine($"{name} is attacking.");
+        }
+    }
+}
--- ConsoleAppLearning-2023-2024-autumn/Program.cs
+++ ConsoleAppLearning-2023-2024-autumn/Program.cs
@@ -2,21 +2,36 @@
 using ConsoleAppLearning_2023_2024_autumn;
 using System.ComponentModel;
 
-var wiz1 = new Wizard();
-var wiz2 = new Wizard();
+var wiz1 = new Wizard
+{
+    name = "Gandalf",
+    age = 200,
+    health = 100,
+    energy = 500
+};
 
-wiz1.name = "Gendalf";
-wiz1.age = 200;
-wiz1.health = 100;
-wiz1.energy = 500;
+var wiz2 = new Wizard
+{
+    name = "Dumbledore",
+    age = 105,
+    health = 100,
+    energy = 100
+};
 
-wiz2.name = "Dubledorf";
-wiz2.age = 105;
-wiz2.health = 100;
-wiz2.energy = 100;
+var drag1 = new Dragon
+{
+    name = "Smaug",
+    age = 5000,
+    health = 100,
+    energy = 100
+};
+
 
 wiz1.Talk();
 wiz1.Heal();
 
 wiz2.Talk();
 wiz2.Heal();
+
+drag1.Talk();
+drag1.Attack();
